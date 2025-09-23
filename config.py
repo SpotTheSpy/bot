ea162113dffd276bb6ce832852b5a2c3e5a0fbb9
Plummy <plummybeatsoff@gmail.com>
2025-09-23 11:57:01 +0200

    Initial commit

---
+++ config.py
@@ -0,0 +1,9 @@
+from pydantic import SecretStr
+from pydantic_settings import BaseSettings
+
+
+class Config(BaseSettings):
+    telegram_bot_token: SecretStr
+    telegram_secret: SecretStr
+
+    redis_dsn: SecretStr | None = None
