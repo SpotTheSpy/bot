35734de78a35ba38901b9d26da5f30f653dd2b8f
Plummy <plummybeatsoff@gmail.com>
2025-09-25 00:20:06 +0200

    Really did

--- app/data/secret_words_controller.py
+++ app/data/secret_words_controller.py
@@ -1,18 +1,9 @@
 from json import loads
-from typing import Dict, Any
 
 from aiogram.utils.i18n import gettext as _
 
 
 class SecretWordsController:
-    _SECRET_WORDS: Dict[str, Any] | None = None
-
-    @classmethod
-    def get_secret_word(
-            cls,
-            secret_word: str
-    ) -> str:
-        if cls._SECRET_WORDS is None:
-            cls._SECRET_WORDS = loads(_("data.secret_words"))
-
-        return cls._SECRET_WORDS.get(secret_word, secret_word)
+    @staticmethod
+    def get_secret_word(secret_word: str) -> str:
+        return loads(_("data.secret_words")).get(secret_word, secret_word)
