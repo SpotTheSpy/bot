286d03d48d3d1df12f70c29d50c646c831b5d14a
Plummy <plummybeatsoff@gmail.com>
2025-10-04 16:48:36 +0200

    Fix parameters

--- app/parameters.py
+++ app/parameters.py
@@ -1,6 +1,4 @@
 class Parameters:
-    MIN_PLAYER_AMOUNT: int = 2
+    MIN_PLAYER_AMOUNT: int = 3
     MAX_PLAYER_AMOUNT: int = 8
     DEFAULT_PLAYER_AMOUNT: int = 4
-
-    API_POLLING_TIMEOUT: float = 2.5
