281fad445c96d04e51dd4ebd7f68b189b64d81ed
Plummy <plummybeatsoff@gmail.com>
2025-10-15 17:32:40 +0200

    Document middlewares

--- app/middlewares/i18n.py
+++ app/middlewares/i18n.py
@@ -7,10 +7,24 @@ from app.models.bot_user import BotUser
 
 
 class UserI18nMiddleware(SimpleI18nMiddleware):
+    """
+    Middleware from retrieving user locale from a BotUser object in workflow data.
+
+    Must be set up after user middleware.
+
+    If BotUser object is not specified, middleware returns locale determined by a telegram language code.
+    """
+
     def __init__(
             self,
             i18n: I18n
     ) -> None:
+        """
+        Initialize middleware.
+
+        :param i18n: I18n object to initialize simple I18n middleware.
+        """
+
         super().__init__(i18n)
 
     async def get_locale(
@@ -18,6 +32,14 @@ class UserI18nMiddleware(SimpleI18nMiddleware):
             event: TelegramObject,
             data: Dict[str, Any]
     ) -> str:
+        """
+        Retrieve locale from a BotUser object.
+
+        :param event: Telegram event object.
+        :param data: Workflow data.
+        :return: Retrieved locale.
+        """
+
         user: BotUser | None = data.get("user")
 
         if user is None or user.locale is None:
